7cb39c5827bc356c1ae297104ee6d99bbce76037
001285489 <001285489@N206B1.labs.neit.edu>
2013-10-23 22:04:31 -0400

    simple form

--- week3/index.php
+++ week3/index.php
@@ -1,34 +1,51 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+ 
         <?php
-        // put your code here
+        $dbh = new PDO("mysql:host=localhost;port=3306;dbname=phplab","root","");
+        $stmt = $dbh->prepare('SELECT * FROM week3');
+        $stmt->execute();
         
-        //echo $_POST["fullname"];
-        //print_r($_POST);
+        $result = $stmt->fetchAll(); // fetachall() will get all the data and fill it into an array
         
-        $fullname = "";
-        $email = "";
-        $comments = "";
-        
-        
-        
-        
+        if(count($result))
+        {
+            echo "<table border='1'>"; //starts the table
+            
+            foreach($result as $row) //fills the table row by row
+            {
+                
+                echo "<tr> <td>"; //creats new row in the table
+                echo $row["fullname"]; //full name is filled into the table
+                echo "</td> <td>";
+                echo $row["email"]; // email is filled into the table
+                echo "</td> <td>";
+                echo $row["comments"]; //comments is filled into the table
+                echo "</td> </tr>"; // ends the row
+
+                //print_r($row);
+                //echo"<br />";
+            }
+            echo "</table>"; // ends the table
+        }
+        else
+        {
+            echo "No rows returned.";
+        }
+
+
         ?>
         
-        <form name="mainform" action="index.php" method="post">
-            Full Name: <input type="text" name="fullname" value="<?php echo $fullname;?>"/> <br />
-            Email: <input type="text" name="email" value="<?php echo $email;?>"/> <br />
-            Comments: <br /><textarea name="comments" cols="10" rows="5"><?php echo $comments;?></textarea> <br />
+        <form name="mainform" action="processform.php" method="post">
+            
+            Full Name: <input type="text" name="fullname" value=""/> <br />
+            Email: <input type="text" name="email" value=""/> <br />
+            Comments: <br /><textarea name="comments" cols="10" rows="5"></textarea> <br />
             
             <input type="submit" value="submit" />
             
--- week3/processform.php
+++ week3/processform.php
@@ -1,28 +1,49 @@
 <?php
 
-if(count($_POST))
+    $fullname = "";
+    $email = "";
+    $comments = "";
+
+    if(count($_POST))
+    {
+        if(array_key_exists("fullname", $_POST))
         {
-            if(array_key_exists("fullname", $_POST))
-            {
-                $fullname = $_POST["fullname"];
-            }
-            
-            if(array_key_exists("email", $_POST))
-            {
-                $email = $_POST["email"];
-            }
-            
-            if(array_key_exists("comments", $_POST))
-            {
-                $comments = $_POST["comments"];
-            }
+            $fullname = $_POST["fullname"];
         }
 
+        if(array_key_exists("email", $_POST))
+        {
+            $email = $_POST["email"];
+        }
 
+        if(array_key_exists("comments", $_POST))
+        {
+            $comments = $_POST["comments"];
+        }
+    } //-- end of count if statment --
 
+    if(!empty($fullname) && !empty($email) && !empty($comments))
+    {
+        $dbh = new PDO("mysql:host=localhost;port=3306;dbname=phplab","root","");
 
-
-
-
+        try
+        {
+            $stmt = $dbh->prepare('insert into week3 set fullname = :fullnameValue, email = :emailValue, comments = :commentsValue'); // this is preparing the statment
+            $stmt->bindParam(':fullnameValue', $fullname, PDO::PARAM_STR); //binds information from the form to the variable in the statment
+            $stmt->bindParam(':emailValue', $email, PDO::PARAM_STR);
+            $stmt->bindParam(':commentsValue', $comments, PDO::PARAM_STR);
+            $stmt->execute(); // executes the statment that was prepared
+            
+            echo "<h3>Info Submited</h3><p><a href='index.php'>Back to form</a></p>";
+        } // --end of try --
+        catch (PDOException $e) 
+        {
+            echo "DataBase Error";
+        } // -- end of catch --
+    } 
+    else 
+    {
+         echo "<h3>Info NOT Submited</h3><p><a href='index.php'>Back to form</a></p>";  
+    } //-- end of empty try catch if statment --
 
 ?>
