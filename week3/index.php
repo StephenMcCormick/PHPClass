d7d419419a1ecca2be8e4873a29ee0362bc96412
001285489 <001285489@N206B1.labs.neit.edu>
2013-10-23 20:12:26 -0400

    simple form

--- week3/index.php
+++ week3/index.php
@@ -14,13 +14,38 @@ and open the template in the editor.
         // put your code here
         
         //echo $_POST["fullname"];
-        print_r($_POST)
+        //print_r($_POST);
+        
+        $fullname = "";
+        $email = "";
+        $comments = "";
+        
+        if(count($_POST))
+        {
+            if(array_key_exists("fullname", $_POST))
+            {
+                $fullname = $_POST["fullname"];
+            }
+            
+            if(array_key_exists("email", $_POST))
+            {
+                $email = $_POST["email"];
+            }
+            
+            if(array_key_exists("comments", $_POST))
+            {
+                $comments = $_POST["comments"];
+            }
+        }
+        
+        
         ?>
         
         <form name="mainform" action="index.php" method="post">
-            Full Name: <input type="text" name="fullname" value=""/> <br />
-            Email: <input type="text" name="email" value=""/> <br />
-            Comments: <br /><textarea name="comments" cols="10" rows="5"> </textarea> <br />
+            Full Name: <input type="text" name="fullname" value="<?php echo $fullname;?>"/> <br />
+            Email: <input type="text" name="email" value="<?php echo $email;?>"/> <br />
+            Comments: <br /><textarea name="comments" cols="10" rows="5"><?php echo $comments;?></textarea> <br />
+            
             <input type="submit" value="submit" />
             
         </form>
