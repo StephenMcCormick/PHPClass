f84ea632a38e8bda8b7eae5fd272078d62a23786
001285489 <001285489@N206B1.labs.neit.edu>
2013-11-13 21:20:26 -0500

    week 6 is a review of basic concepts with multi form introduction and database push and pull

--- week6/itemF.php
+++ week6/itemF.php
@@ -1,3 +1,4 @@
+<?php include 'dependency.php'; ?>
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -28,11 +29,54 @@ and open the template in the editor.
          * before excuting
          */
         
-        
-        
+        if(count($_POST))
+        {
+            if( Validator::zipIsValid($_POST["zip"])
+                    && Validator::nameIsValid($_POST["name"])
+                    && Validator::stringIsValid($_POST["state"])
+                    && Validator::stringIsValid($_POST["city"])
+                    && Validator::stringIsValid($_POST["address"]) )
+            {
+                
+                    $db = new PDO(Config::DB_DNS, Config::DB_USER, Config::DB_PASSWORD);
+                
+                   
+                
+                if ( null != $db ) 
+                {
+                    $stmt = $db->prepare('insert into addressbook set address = :addressValue, '
+                            . 'city = :cityValue, '
+                            . 'state = :stateValue, '
+                            . 'zip = :zipValue, '
+                            . 'name = :nameValue');
+                    $stmt->bindParam(':addressValue', $_POST["address"], PDO::PARAM_STR);
+                    $stmt->bindParam(':cityValue', $_POST["city"], PDO::PARAM_STR);
+                    $stmt->bindParam(':stateValue', $_POST["state"], PDO::PARAM_STR);
+                    $stmt->bindParam(':zipValue', $_POST["zip"], PDO::PARAM_STR);
+                    $stmt->bindParam(':nameValue', $_POST["name"], PDO::PARAM_STR);
+                    $stmt->execute();  
+                }
+                echo "it worked!";
+            }
+            else
+            {
+                echo "nope";
+            }
+        }
         
         
         
         ?>
+        
+        <form id="addressBook" action="#" method="post">
+            address: <input type="text" name="address" value="" /><br />
+            city: <input type="text" name="city" value="" /><br />
+            state: <input type="text" name="state" value="" /><br />
+            zip: <input type="text" name="zip" value="" /><br />
+            name: <input type="text" name="name" value="" /><br />
+            <br />
+            <input type="submit" name="submitAddress" value="Submit" />
+        </form>
+        
     </body>
 </html>
